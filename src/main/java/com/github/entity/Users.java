7fcd5f67006411ba19ab94ec2b8fcfe7218366c5
Sujith Ramanathan <sujithramanathan@gmail.com>
2024-04-07 16:01:58 +0530

    Commit user class

---
+++ src/main/java/com/github/entity/Users.java
@@ -0,0 +1,10 @@
+package com.github.entity;
+
+import java.util.List;
+
+public class Users {
+    private String userId;
+    private char[] password;
+    private String mobileNumber;
+    private List<String> roles;
+}
